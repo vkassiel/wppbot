ff847cf477bcb01fa3480a258c09992ed4c3de94
Vitor Kassiel <kassielalmeida@gmail.com>
2021-01-10 02:17:21 -0300

    Validação na conversão de imagem para sticker

--- src/services/convert.js
+++ src/services/convert.js
@@ -3,19 +3,25 @@ const { decryptMedia } = require('@open-wa/wa-automate')
 class Convert {
 
     async imageToSticker(client, message) {
-        const { from, sender, mimetype, chat } = message
+        const { from, sender, mimetype, chat, type } = message
 
-        // Message confirming receipt of the image 
-        await client.reply(chat.id, `${sender.pushname}, aguarde um pouco enquanto sua figurinha é gerada.`, message.id, false)
+        if(type != 'image'){
+            await client.reply(chat.id, `${sender.pushname}, com o *!sticker* você converte IMAGEM em STICKER. Para vídeos ou gifs, tente o comando *!gif*`, message.id, false)
+        }else{
+            // Message confirming receipt of the image 
+            await client.reply(chat.id, `${sender.pushname}, aguarde um pouco enquanto sua figurinha é gerada.`, message.id, false)
+    
+            // Decrypting message
+            const media = await decryptMedia(message)
+    
+            // Converting to base64
+            let base64 = media.toString('base64')
+    
+            // Re-sending to user in sticker format
+            await client.sendImageAsSticker(from, `data:${mimetype};base64,${base64}`)  
+        }
 
-        // Decrypting message
-        const media = await decryptMedia(message)
 
-        // Converting to base64
-        let base64 = media.toString('base64')
-
-        // Re-sending to user in sticker format
-        await client.sendImageAsSticker(from, `data:${mimetype};base64,${base64}`)
     }
 
     async videoToSticker(client, message) {
