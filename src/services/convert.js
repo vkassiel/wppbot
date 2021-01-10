8b4900ea95419377245f1a2bffc2f446e5a86d26
Vitor Kassiel <kassielalmeida@gmail.com>
2021-01-10 03:27:54 -0300

    Ajustando validação nas conversões

--- src/services/convert.js
+++ src/services/convert.js
@@ -5,20 +5,20 @@ class Convert {
     async imageToSticker(client, message) {
         const { from, sender, mimetype, chat, type } = message
 
-        if(type != 'image'){
+        if (type != 'image') {
             await client.reply(chat.id, `${sender.pushname}, com o *!sticker* você converte IMAGEM em STICKER. Para vídeos ou gifs, tente o comando *!gif*`, message.id, false)
-        }else{
+        } else {
             // Message confirming receipt of the image 
             await client.reply(chat.id, `${sender.pushname}, aguarde um pouco enquanto sua figurinha é gerada.`, message.id, false)
-    
+
             // Decrypting message
             const media = await decryptMedia(message)
-    
+
             // Converting to base64
             let base64 = media.toString('base64')
-    
+
             // Re-sending to user in sticker format
-            await client.sendImageAsSticker(from, `data:${mimetype};base64,${base64}`)  
+            await client.sendImageAsSticker(from, `data:${mimetype};base64,${base64}`)
         }
 
 
